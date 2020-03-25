a57d85d06a7b821e73f6002b006dfd38c17e990d
Kaoru Kawashima <altoinu@users.noreply.github.com>
2020-03-24 20:17:40 -0400

    do quicksort letters in given text

--- src/index.js
+++ src/index.js
@@ -1,11 +1,68 @@
+function split(text) {
+
+	// using last char as pivot
+	var pivot = text.charAt(text.length - 1);
+
+	// check each letter and put separate to ones before and after pivot 
+	var before = [];
+	var after = [];
+
+	for (var i = 0; i < text.length - 1; i++) {
+
+		let targetChar = text.charAt(i);
+
+		if (targetChar <= pivot)
+			before.push(targetChar);
+		else
+			after.push(targetChar);
+
+	}
+
+	var result = {
+		pivot: pivot,
+		before: before.join(''),
+		after: after.join('')
+	};
+
+	//console.log(result);
+
+	return result;
+
+}
+
+function quicksort(text) {
+
+	// If 1 or 0 chars, no sorting needed
+	if (text.length <= 1)
+		return text;
+
+	var splitText = split(text);
+
+	// recursively sort before and after texts
+	var sortedBeforeText = quicksort(splitText.before);
+	var sortedAfterText = quicksort(splitText.after);
+
+	// then combine results
+	var sortCompletedText = sortedBeforeText + splitText.pivot + sortedAfterText;
+	//console.log(sortCompletedText);
+
+	return sortCompletedText;
+
+}
+
 function getLettersSorted(text) {
 
-	// lower case
-	var sortText = text.toLowerCase();
-	// remove punctuations (replace everything that's not a letter)
-	sortText = sortText.replace(/[^\w]/gi, '')
+	console.log(text);
 
-	return 'hello ' + sortText;
+	// lower case
+	var sortTargetText = text.toLowerCase();
+	// remove punctuations (replace everything that's not a letter)
+	sortTargetText = sortTargetText.replace(/[^\w]/gi, '')
+
+	var sortCompletedText = quicksort(sortTargetText);
+	console.log(sortCompletedText);
+
+	return sortCompletedText;
 
 };
 
--- test/test.js
+++ test/test.js
@@ -1,18 +1,17 @@
 var assert = require('assert');
 var getLettersSorted = require('../src/index.js');
 
-/*
-describe('Array', function () {
-	describe('#indexOf()', function () {
-		it('should return -1 when the value is not present', function () {
-			assert.equal([1, 2, 3].indexOf(4), -1);
-		});
+describe('getLettersSorted', () => {
+	it('should return letters, without punctuation and in lower case, sorted alphabetically', () => {
+		assert.equal(getLettersSorted('John, Smith!'), 'hhijmnost');
 	});
 });
-*/
 
 describe('getLettersSorted', () => {
-	it('should return text without punctuation and in lower case', () => {
-		assert.equal(getLettersSorted('John, Smith!'), 'hello johnsmith');
+	it('should return letters, without punctuation and in lower case, sorted alphabetically', () => {
+		assert.equal(
+			getLettersSorted('When not studying nuclear physics, Bambi likes to play beach volleyball.'),
+			'aaaaabbbbcccdeeeeeghhhiiiiklllllllmnnnnooopprsssstttuuvwyyyy'
+		);
 	});
 });
